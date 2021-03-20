2a9f0623e1f3a4a07229d9291c80043fbd3c2081
bhaskarjd3e <bhaskarjd@3esofttech.com>
2021-03-20 22:59:21 +0530

    youtube key changed

--- src/apiInterceptor.js
+++ src/apiInterceptor.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const http = axios.create({
     baseURL:"https://www.googleapis.com/youtube/v3/",
     params:{
-        key:"AIzaSyDHLa8iEjagehgXlKVXK-0_Riw7XbkC4ac"
+        key:"AIzaSyCUmLFPhEXK_kxIvRoZ72js8OsMJepPD2s"
     }
 })
 
